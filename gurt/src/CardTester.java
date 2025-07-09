15d2289ad1a95f546eaaa626c0c4fd1453782b23
theoliverboliver <olliepeluso@gmail.com>
2025-07-09 15:14:21 +0000

    card shii

--- gurt/src/CardTester.java
+++ gurt/src/CardTester.java
@@ -2,7 +2,9 @@ import java.util.ArrayList;
 public class CardTester {
 
     public static void main(String[] args) {
-      Card yo= new Card("spades", 1);
-      System.out.println(yo);
+        Deck hi= new Deck();
+        hi.shuffle();
+        hi.printDeck();
+       
     }
 }
--- gurt/src/Deck.java
+++ gurt/src/Deck.java
@@ -3,22 +3,38 @@ public class Deck {
     private ArrayList<Card> deck;
 
     public Deck(){
+        deck = new ArrayList<Card>();
          for(int x=1; x<5; x++){
             
             for(int y=1; y<14; y++){
                if(x==1){
                 deck.add(new Card("clubs", y));
                } 
-                if(x==2){
+               else if(x==2){
                 deck.add(new Card("diamonds", y));
                }     
-                if(x==3){
+                else if(x==3){
                 deck.add(new Card("hearts", y));
                }    
-               else{
+               else {
                 deck.add(new Card("spades", y));
                }
             }
         }
     }
+    public void shuffle(){
+       
+        for(int x=0; x<52; x++){
+            int rand=(int)(Math.random()*52);
+            Card save=deck.get(rand);
+            Card y= deck.get(x);
+            deck.set(rand, y);
+            deck.set(x, save);
+        }
+    }
+    public void printDeck(){
+        for(Card xvur: deck){
+            System.out.println(xvur);
+        }
+    }         
 }
