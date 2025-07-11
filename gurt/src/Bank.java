cf8814f6fe867a0d588074bd7a34a6233f925b98
theoliverboliver <olliepeluso@gmail.com>
2025-07-11 14:00:03 +0000

    we f8ucked up

---
+++ gurt/src/Bank.java
@@ -0,0 +1,10 @@
+public class Bank {
+    private int money;
+    private String name;
+    private static int numofaces
+    public void acc(int money, String name, int numofaces){
+        this.money=money;
+        this.name=name;
+        this.numofaces
+    }
+}
--- gurt/src/CardTester.java
+++ gurt/src/CardTester.java
@@ -29,46 +29,80 @@ public class CardTester {
         }
        Card x = playerone.remove(0);
        Card z = playertwo.remove(0);
-       if(x.getNumber() < z.getNumber()){
+       if(x.getNumber() > z.getNumber()){
         System.out.println("you win that round");
         
           playerone.add(x);
           playerone.add(z);
        }
-       else if(x.getNumber() > z.getNumber()){
+       else if(x.getNumber() < z.getNumber()){
         System.out.println("you lose that round");
           playertwo.add(x);
           playertwo.add(z);
        }
        else{
+        System.out.println("war time");
         ArrayList<Card> temp = new ArrayList<>();
         System.out.println("its a tie!!! each side draw four cards, whoevers fourth card is bigger wins the tie.");
-        if(playerone.size()<4){
-          System.out.println("you do not have enough cards for war, so player two wins.");
-          break;
+        while(true){
+          if(playerone.size()<4){
+            System.out.println("you do not have enough cards for war, so player two wins.");
+            break;
+          }
+          if(playertwo.size()<4){
+            System.out.println("player two does not have enough cards for war, so you win.");
+            break;
+          }
+          temp.add(playerone.remove(0));
+          temp.add(playerone.remove(0));
+          temp.add(playerone.remove(0));
+      
+          temp.add(playertwo.remove(0));
+          temp.add(playertwo.remove(0));
+          temp.add(playertwo.remove(0));
+
+          Card a = playerone.remove(0);
+          Card c = playertwo.remove(0);
+          System.out.println("Temp:");
+          for(Card h: temp) {
+            System.out.println(h);
+          }
+          
+          System.out.println("A: "+ a);
+          System.out.println("C: " + c);
+          System.out.println("your opponent drew a " + c + ", you drew a " + a);
+          if(a.getNumber() > c.getNumber()){
+            System.out.println("you won the war!");
+            for(Card blemp: temp  ){
+              playerone.add(blemp);
+            }
+            playerone.add(a);
+            playerone.add(c);
+           
+            break;
+          }
+          else if(a.getNumber() < c.getNumber()){
+            System.out.println("you lost the war");
+            for(Card blemp: temp  ){
+              playertwo.add(blemp);
+            }
+            playertwo.add(a);
+            playertwo.add(c);
+            break;
+          }
+          else{
+            temp.add(a);
+            temp.add(c);
+            System.out.println("well ill be damned, we got a stacked war!!!");
+          }
         }
-        if(playertwo.size()<4){
-          System.out.println("player two does not have enough cards for war, so you win.");
-          break;
-        }
-        temp.add(playerone.remove(0));
-        temp.add(playerone.remove(0));
-        temp.add(playerone.remove(0));
-     
-        temp.add(playertwo.remove(0));
-        temp.add(playertwo.remove(0));
-        temp.add(playertwo.remove(0));
-       
-        if(x.getNumber() > z.getNumber()){
-          System.out.println("you won the war!");
-          playerone.add(temp);
-        }
-       }
+      }
           String moveon = s.nextLine();
+           System.out.println("you have " +  playerone.size() + " cards.");
+         System.out.println("your opponent has " + playertwo.size() + " cards.");
         System.out.println("player one, your card is the " + playerone.get(0));
         System.out.println("player two's card is the " + playertwo.get(0));
-      System.out.println("you have " +  playerone.size() + " cards.");
-         System.out.println("your opponent has " + playertwo.size() + " cards.");
+     
       }
 
         }
